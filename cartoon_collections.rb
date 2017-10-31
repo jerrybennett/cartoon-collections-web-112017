7a07a3e83b3c61ad78b533891a0f2f3d3f360f94
Jerry Bennett <jerrysbusiness@gmail.com>
2017-10-31 19:14:05 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -21,6 +21,6 @@ def find_the_cheese(arr)
   cheese_types = ["cheddar", "gouda", "camembert"]
   match = (arr & cheese_types)
   if match != nil
-    print "#{match[0]}"
+    match[0].to_s
   end
 end
