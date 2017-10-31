d6bfe76492360cd25192a83e1250411ac03146c5
Jerry Bennett <jerrysbusiness@gmail.com>
2017-10-31 17:56:25 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -5,7 +5,7 @@ def roll_call_dwarves(dwarves)
 end
 
 def summon_captain_planet(arr)
-  arr.mapmap(&:upcase + "!")
+  arr.map(&:upcase + "!") 
 end
 
 def long_planeteer_calls# code an argument here
