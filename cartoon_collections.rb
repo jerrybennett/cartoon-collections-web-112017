928b3e0a592387ff20139b3404d424f20b80b129
Jerry Bennett <jerrysbusiness@gmail.com>
2017-10-31 19:32:49 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -20,9 +20,9 @@ def find_the_cheese(arr)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
   match = (arr & cheese_types)
-  if match != nil
+  if match == nil
+    return nil
+  elsif match != nil
     match[0].to_s
-  else
-    nil
   end
 end
