7655587ed4084830d0e3d713c65fada8bbbe930e
Jerry Bennett <jerrysbusiness@gmail.com>
2017-10-31 17:40:35 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,6 +1,7 @@
 def roll_call_dwarves(dwarves)
   dwarves.each_with_index(1) {|name, num|
-    puts "#{num}. #{name}"}
+    prints "#{num}. #{name}"
+  }
 end
 
 def summon_captain_planet# code an argument here
